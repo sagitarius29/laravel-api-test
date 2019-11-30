7c3334ea91dc765328d26983ef474cac4341c556
Adolfo Cuadros <ronnie.adolfo@gmail.com>
2019-11-30 13:28:19 -0500

    Initial Commit

---
+++ tests/Feature/ApiRequesterTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ApiRequesterTest extends TestCase
+{
+    public function it_can_schedule_job_for_requesting_data()
+    {
+        
+    }
+}
