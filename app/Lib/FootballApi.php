159725c9e3be572d6fdee4b6ae467d8d17b5471a
Adolfo Cuadros <ronnie.adolfo@gmail.com>
2019-11-30 14:52:42 -0500

    create app tables

---
+++ app/Lib/FootballApi.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Lib;
+
+use GuzzleHttp\Client;
+
+class FootballApi
+{
+    protected $token;
+    protected $client;
+    protected $baseUri = 'http://api.football-data.org/';
+
+    public function __construct($version, $token = null)
+    {
+        $this->token = $token ?? env('FOOTBALL_API_TOKEN', '');
+        $this->client = new Client([
+            'headers' => [
+                'X-Auth-Token' => $this->token
+            ],
+            'base_uri' => $this->baseUri.$version.'/'
+        ]);
+    }
+
+    public function get($uri)
+    {
+        $response = $this->client->get($uri);
+        $contents = $response->getBody()->getContents();
+
+        return json_decode($contents, true);
+    }
+}
--- database/migrations/2019_11_30_181403_create_app_tables.php
+++ database/migrations/2019_11_30_181403_create_app_tables.php
@@ -13,8 +13,45 @@ class CreateAppTables extends Migration
      */
     public function up()
     {
-        Schema::create('app_tables', function (Blueprint $table) {
-            $table->bigIncrements('id');
+        Schema::create('competitions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('code')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('teams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('short_name')->nullable();
+            $table->string('tla')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('website')->nullable();
+            $table->string('email')->nullable();
+            $table->integer('founded')->nullable();
+            $table->string('club_colors')->nullable();
+            $table->string('venue')->nullable();
+
+            $table->timestamps();
+        });
+
+        Schema::create('competitions_teams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->increments('competition_id')->unsigned();
+            $table->increments('team_id')->unsigned();
+        });
+
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('team_id')->unsigned();
+            $table->string('name');
+            $table->string('position')->nullable();
+            $table->integer('shirtNumber')->nullable();
+
+            $table->foreign('team_id')
+                ->references('id')->on('teams');
+
             $table->timestamps();
         });
     }
@@ -26,6 +63,7 @@ class CreateAppTables extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('app_tables');
+        Schema::dropIfExists('players');
+        Schema::dropIfExists('competitions_teams');
     }
 }
